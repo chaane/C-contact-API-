5125a52380b8765a2123b172ac260c438760053a
chaane <49711533+chaane@users.noreply.github.com>
2023-03-15 08:40:21 +0200

    Add files via upload

---
+++ Modules/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Modules
+{
+    public class Contact
+    {
+        public Guid id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    
+    }
+}
